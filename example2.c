db80412eff016cce8d7e2fa83590cae81bbe5775
rxn7 <rotthin_dev@protonmail.com>
2023-07-24 10:29:08 +0200

    Replaced spaces with tabs

--- example2.c
+++ example2.c
@@ -2,16 +2,16 @@
 #include "redflag.h"
 
 int main(int argc, char *argv[]) {
-  rf_context_t ctx;
-  rf_init_context(&ctx);
+	rf_context_t ctx;
+	rf_init_context(&ctx);
 
-  rf_flag_t *age_flag = rf_flag_int(&ctx, "age", "Your age", 16);
-  rf_flag_t *name_flag = rf_flag_str(&ctx, "name", "Your name", "Maciej");
+	rf_flag_t *age_flag = rf_flag_int(&ctx, "age", "Your age", 16);
+	rf_flag_t *name_flag = rf_flag_str(&ctx, "name", "Your name", "Maciej");
 
-  rf_parse_flags(&ctx, argc, argv);
-  rf_print_all_flags(&ctx);
+	rf_parse_flags(&ctx, argc, argv);
+	rf_print_all_flags(&ctx);
 
-  rf_free_context(&ctx);
+	rf_free_context(&ctx);
 
-  return 0;
+	return 0;
 }
